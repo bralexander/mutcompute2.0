b8ed349564ac6bf8eea6e38b98016625c09e6dce
Brad Alexander <bralexander@live.com>
2021-03-25 10:20:03 -0500

    working with import*

--- src/components/viewer.js
+++ src/components/viewer.js
@@ -1,18 +1,26 @@
 import React, {useEffect} from 'react';
-import { Stage } from 'ngl';
+import * as NGL from 'ngl'
 
 
 // need to install react-document-meta in npm and import to update meta tags
 
 const Viewer = () => {
 useEffect (() => {
-    var stage = new Stage("viewport");
+    NGL.DatasourceRegistry.add("data", new NGL.StaticDatasource("/data/"))
+
+    var stage = new NGL.Stage("viewport");
     stage.loadFile("rcsb://1crn", {defaultRepresentation: true});
+    
+    
+    window.addEventListener( "resize", function( event ){
+        stage.handleResize();
+    }, false );
     
 });
 
     return (
         <div id="viewport" style={{width:"100%", height:"100vh"}}>
+           
         </div>
     )
 }
