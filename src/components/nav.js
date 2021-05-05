dc1457e931bbc10299a8a3efac452c8de853a1d8
Brad Alexander <bralexander@live.com>
2021-05-05 16:26:28 -0500

    updated nav for home page

--- src/components/nav.js
+++ src/components/nav.js
@@ -10,7 +10,7 @@ const Nav = (props) => {
             <h3 className="float-md-start mb-0">MutCompute.com</h3>
             <nav className="nav nav-masthead justify-content-center float-md-end">
               <a className="nav-link active" aria-current="page" href="/">Home</a>
-              <a className="nav-link text-muted" href="/FAQ">FAQ</a>
+              <a className="nav-link text-muted" href="/register">Register</a>
               <a className="nav-link text-muted" href="/login/">Login</a>
             </nav>
           </header>
