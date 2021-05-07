56caded3c602e8754353c28c06e407fba39b81a8
Brad Alexander <bralexander@live.com>
2021-05-07 11:45:03 -0500

    nav changes if logged in

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,22 +1,44 @@
 import React from "react";
+import { useAuth } from "../auth";
+
+
 
 const Nav = (props) => {
 // bootstrap navbar
+
+const [logged] = useAuth()
+
     return(
-      <div>
+       <div>
         <div className="nav-cont d-flex h-100 text-center text-white">
         <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
           <header>
-            <h3 className="float-md-start mb-0">MutCompute.com</h3>
+            <a href="/"><h3 className="float-md-start mb-0">MutCompute.com</h3></a>
+            {!logged?
             <nav className="nav nav-masthead justify-content-center float-md-end">
-              <a className="nav-link active" aria-current="page" href="/">Home</a>
               <a className="nav-link text-muted" href="/register">Register</a>
               <a className="nav-link text-muted" href="/login/">Login</a>
+              <a className="nav-link active" aria-current="page" href="/">Home</a>
             </nav>
+            :
+            <nav className="nav nav-masthead justify-content-center float-md-end">
+            <a className="nav-link text-muted"  href="/NN/">Predict</a>
+            <a className="nav-link text-muted" href="/viewer2">View</a>
+            <button className="nav-link dropdown-toggle text-muted" id="navbarDarkDropdownMenuLink"
+             data-bs-toggle="dropdown" aria-expanded="false">
+              Menu
+            </button>
+            <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+              <li><a className="dropdown-item" href="/login">logout</a></li>
+              <li><a className="dropdown-item" href="/FAQ">FAQ</a></li>
+            </ul>
+          </nav>
+          }
           </header>
         </div>
         </div>
       </div>
+      
     )
 }
 
