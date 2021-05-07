903283f70fe2a262c1b9e43c521dd121a2fe68fa
Brad Alexander <bralexander@live.com>
2021-05-06 22:02:01 -0500

    added private route -- move to different folder

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import React from "react"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import { BrowserRouter as Router} from "react-router-dom"
+import { Route, Switch, Redirect } from "react-router-dom"
+import {useAuth} from "./auth"
+
 import Cover from "./components/cover"
 import Nav from "./components/nav"
 import Footer from "./components/footer"
 import Login from "./components/login"
-import Viewer from "./components/viewer"
+//import Viewer from "./components/viewer"
 import Compvis2 from "./components/compvis2"
-import CompViewer from "./components/compvisViewer"
+//import CompViewer from "./components/compvisViewer"
 import Structure from "./components/structure"
 import Register from "./components/register2"
 import NNPage from "./components/nnPage"
@@ -15,7 +18,16 @@ import FAQ from "./components/faq"
 
 
 
+const  PrivateRoute = ({component: Component, ...rest }) => {
+  const [logged] = useAuth();
 
+  return <Route {...rest} render={(props) => (
+      logged
+      ? <Component {...props} />
+      : <Redirect to='/login'/>
+  )} />
+
+}
 
 
 function App() {
@@ -27,11 +39,11 @@ function App() {
             <Route path="/" exact component={() => <Cover />} />
             <Route path="/login" exact component={() => <Login />} />
             <Route path="/viewer2" exact component={() => <Compvis2 />} />
-            <Route path="/viewer" exact component={() => <CompViewer />} />
+            {/* <Route path="/viewer" exact component={() => <CompViewer />} /> */}
             <Route path="/struc" exact component={() => <Structure />} />
-            <Route path="/ngl" exact component={() => <Viewer />} />
+            {/* <Route path="/ngl" exact component={() => <Viewer />} /> */}
             <Route path="/register" exact component={() => <Register />} />
-            <Route path="/nn" exact component={() => <NNPage />} />
+            <PrivateRoute path="/nn" component={NNPage} />
             <Route path="/faq" exact component={() => <FAQ />} />
           </Switch>
         <Footer />
