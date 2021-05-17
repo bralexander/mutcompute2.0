d2f47a0af5a5f1c15cfed4d8cd92234b0220c1be
Brad Alexander <bralexander@live.com>
2021-05-17 16:56:30 -0500

    removed max's wrapper

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//import { AuthContextProvider } from './store/auth-context'
 
 ReactDOM.render(
-  <React.StrictMode>
+  //<AuthContextProvider>
+    <React.StrictMode>
     <App />
   </React.StrictMode>,
+  //</AuthContextProvider>,
   document.getElementById('root')
 );
 
