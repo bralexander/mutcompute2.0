0871ead89675f34134418d25800cb2802adca0fa
Brad Alexander <bralexander@live.com>
2021-06-07 22:54:26 -0500

    removed old commented code

--- api/app/routes.py
+++ api/app/routes.py
@@ -4,92 +4,22 @@ import flask_sqlalchemy
 import flask_praetorian
 import flask_cors
 
+import jwt
+
 from app import app
 from app.email import send_password_reset_email
 from app.models import User
 from flask_login import current_user, login_user
 
+from time import time
+
+
 db = flask_sqlalchemy.SQLAlchemy()
-# guard = flask_praetorian.Praetorian()
 cors = flask_cors.CORS()
-
-
-# A generic user model that might be used by an app powered by flask-praetorian
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     email = db.Column(db.Text, unique=True)
-#     #email = db.Column(db.Text, unique=True)
-#     password = db.Column(db.Text)
-#     roles = db.Column(db.Text)
-#     is_active = db.Column(db.Boolean, default=True, server_default='true')
-
-#     @property
-#     def rolenames(self):
-#         try:
-#             return self.roles.split(',')
-#         except Exception:
-#             return []
-
-#     @classmethod
-#     def lookup(cls, email):
-#         return cls.query.filter_by(email=email).one_or_none()
-
-#     @classmethod
-#     def identify(cls, id):
-#         return cls.query.get(id)
-
-#     @property
-#     def identity(self):
-#         return self.id
-
-#     def get_reset_password_token(self, expires_in=600):
-#         return guard.encode_jwt_token(self.id, override_access_lifespan=None, override_refresh_lifespan=None, bypass_user_check=False, is_registration_token=False, is_reset_token=False)
-#         # (
-#         #     {'reset_password': self.id, 'exp': time() + expires_in},
-#         #     app.config['SECRET_KEY'], algorithm='HS256')
-
-#     @staticmethod
-#     def verify_reset_password_token(token):
-#         try:
-#             id = guard.read_token(token)
-#             # , app.config['SECRET_KEY'],
-#             #                 algorithms=['HS256'])['reset_password']
-#         except:
-#             return
-#         return User.query.get(id)
-
-#     def is_valid(self):
-#         return self.is_active
-
-
-# Initialize flask app for the example
-# app = flask.Flask(__name__)
-# app.debug = True
-# app.config['SECRET_KEY'] = 'top secret'
-# app.config['JWT_ACCESS_LIFESPAN'] = {'hours': 24}
-# app.config['JWT_REFRESH_LIFESPAN'] = {'days': 30}
+# guard = flask_praetorian.Praetorian()
 
 # # Initialize the flask-praetorian instance for the app
 #guard.init_app(app, User)
-
-# # Initialize a local database for the example
-# app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(os.getcwd(), 'database.db')}"
-# db.init_app(app)
-
-# # Initializes CORS so that the api_tool can talk to the example app
-# cors.init_app(app)
-
-# Add users for the example
-## if this isn't here, db doesn't recognize login
-# with app.app_context():
-#     db.create_all()
-#     if db.session.query(User).filter_by(email='danny.diaz@utexas.edu').count() < 1:
-#         db.session.add(User(
-#           email='danny.diaz@utexas.edu',
-#           password=guard.hash_password('smokesmoke'),
-#           roles='admin'
-#             ))
-#     db.session.commit()
 
 
 # Set up some routes for the example
@@ -104,7 +34,7 @@ def login():
     Logs a user in by parsing a POST request containing user credentials and
     issuing a JWT token.
     .. example::
-       $ curl http://localhost:5000/api/login -X POST \
+       $ curl http://localhost:5000/api/login -X POST 
          -d '{"username":"Yasoob","password":"strongpassword"}'
     """
     req = flask.request.get_json(force=True)
