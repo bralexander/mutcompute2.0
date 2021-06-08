a7a5375e604ebbf7078967e34a99604ea957d024
Brad Alexander <bralexander@live.com>
2021-06-07 22:55:55 -0500

    working

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -4,12 +4,14 @@ from flask_mail import Mail
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 mail = Mail(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
 from app import routes, models
 
