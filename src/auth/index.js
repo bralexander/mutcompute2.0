255e3379c217a0e240cfd232afd72e715bbced36
Brad Alexander <bralexander@live.com>
2021-06-15 16:23:30 -0500

    updated fetch route

--- src/auth/index.js
+++ src/auth/index.js
@@ -3,7 +3,7 @@ import {createAuthProvider} from 'react-token-auth';
 export const [useAuth, authFetch, login, logout] =
     createAuthProvider({
             accessToKey: 'access_token',
-            onUpdateToken: (token) => fetch ('/token/refresh', {
+            onUpdateToken: (token) => fetch ('/api/refresh', {
                 method: 'POST',
                 body: token.access_token             
             })
