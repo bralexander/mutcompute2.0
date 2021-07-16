94398bc638e56d634cfbf3418eaa1f892648b013
Brad Alexander <bralexander@live.com>
2021-07-16 15:23:52 -0500

    added .env file for email environment variables: working

--- api/.flaskenv
+++ api/.flaskenv
@@ -1,4 +1,2 @@
 FLASK_APP=api.py
 FLASK_ENV=development
-MAIL_SERVER=localhost
-MAIL_PORT=8025
--- api/config.py
+++ api/config.py
@@ -1,6 +1,6 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-
+load_dotenv = os.path.join(basedir, '.env')
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
