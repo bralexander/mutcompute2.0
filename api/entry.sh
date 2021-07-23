b7aaaabe26903bbc89bd9f4f912804d753eee107
Brad Alexander <bralexander@live.com>
2021-07-23 14:10:48 -0500

    front end and backend communicating and available on localhost

--- api/entry.sh
+++ api/entry.sh
@@ -3,4 +3,8 @@
 # flask db upgrade
 #yarn start-api
 # flask translate compile
-exec gunicorn -b :5000 api:app
+exec gunicorn -b 0.0.0.0:5000 api:app 
+
+# not working
+# source venv/bin/activate
+# exec python -m smtpd -n -c DebuggingServer localhost:8025 
