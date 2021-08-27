7143d7e23277a18d7e27865d03c8158b05c19b18
Brad Alexander <bralexander@live.com>
2021-08-26 20:23:28 -0500

    needed to commit so i could switch branches

---
+++ api/migrations/versions/b376ff15ffcb_added_password_hash_column.py
@@ -0,0 +1,24 @@
+"""added password hash column
+
+Revision ID: b376ff15ffcb
+Revises: 
+Create Date: 2021-08-13 11:12:36.753790
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b376ff15ffcb'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
