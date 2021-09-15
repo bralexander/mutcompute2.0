a49abc86fc620801d9565aa0633ecb92597008ed
Brad Alexander <bralexander@live.com>
2021-09-15 17:10:05 -0500

    not working with ses email server

--- api/config.py
+++ api/config.py
@@ -11,10 +11,5 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Setup for email server
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
+    
     
