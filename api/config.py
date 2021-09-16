fe4315fae25d80b1b5d7ff40ff57da9f8fdc342f
Brad Alexander <bralexander@live.com>
2021-09-15 21:54:38 -0500

    commented out unecessary code

--- api/config.py
+++ api/config.py
@@ -13,8 +13,8 @@ class Config(object):
     # Setup for email server
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    # MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['your-email@example.com']
     
