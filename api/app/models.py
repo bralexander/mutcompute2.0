b318053d59ec0d8aedb79809c772e52816d391b6
Brad Alexander <bralexander@live.com>
2021-09-24 12:22:28 -0500

    models working with added fields

--- api/app/models.py
+++ api/app/models.py
@@ -1,3 +1,4 @@
+from sqlalchemy.sql.schema import DefaultClause
 import jwt
 
 from app import db, login, app
@@ -5,6 +6,11 @@ from time import time
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
+from sqlalchemy.schema import CheckConstraint
+from datetime import datetime, timedelta
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt()
 
 # guard = flask_praetorian.Praetorian()
 
@@ -20,6 +26,22 @@ class User(UserMixin, db.Model):
     last_name = db.Column(db.String(length=255))
     organization = db.Column(db.Text)
     password_hash = db.Column(db.String(128))
+    registered_on = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    confirmation_link_sent_on = db.Column(db.DateTime, nullable=True)
+    email_confirmed = db.Column(db.Boolean, nullable=True, default=False)
+    email_confirmed_on = db.Column(db.DateTime, nullable=True)
+    #queries = db.relationship("NN_Query",backref="user", lazy='dynamic')
+
+    # def __init__(self, first_name, last_name,email,password,organization, confirmation_link_sent_on=None):
+    #     self.first_name = first_name
+    #     self.last_name = last_name
+    #     self.email = email
+    #     self.password = bcrypt.generate_password_hash(password).decode()
+    #     self.registered_on = datetime.now()
+    #     self.organization = organization
+    #     self.confirmation_link_sent_on = confirmation_link_sent_on
+    #     self.email_confirmed = False
+    #     self.email_confirmed_on = None
 
     def __repr__(self):
         return '<User {}>'.format(self.email)
@@ -50,38 +72,123 @@ class User(UserMixin, db.Model):
         except:
             return
         return User.query.get(id)
+
+    # Mutcompute model
+    def __repr__(self):
+        return '<User %s %s <%s> from %s>' % \
+               (self.first_name, self.last_name, self.email, self.organization)
+
+
+    def encode_auth_token(self, user_id):
+        """
+        Generates the Auth Token
+        :return: string
+        """
+        try:
+            payload = {
+                'exp': datetime.utcnow() + timedelta(days=0,minutes=30,seconds=0),
+                'iat': datetime.utcnow(),
+                'sub': user_id
+            }
+
+            return jwt.encode(payload=payload,
+                              key=app.config.get('JWT_SECRET_KEY'),
+                              algorithm='HS256'
+                              )
+
+        except Exception as e:
+            return e
+
+    @staticmethod
+    def decode_auth_token(auth_token):
+        """
+        Validates the auth token
+        :param auth_token:
+        :return: integer|string
+        """
+        try:
+            payload = jwt.decode(auth_token,key=app.config.get('JWT_SECRET_KEY'))
+            return payload['sub']
+
+        except jwt.ExpiredSignatureError:
+            return "Signature Expired. Please login again."
+
+        except jwt.InvalidTokenError:
+            return "Invalid Token. Please login again."
+
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+        app.logger.info('Successfully saved to DB.')
+
+
+    def check_pw(self,form_pw):
+        return bcrypt.check_password_hash(self.password,form_pw)
+
+
+    def check_user_confirmed(self):
+        app.logger.info('check if user confirmed: {}'.format(self.email_confirmed))
+        return self.email_confirmed
+
+
+    def confirm_email(self):
+        self.email_confirmed = True
+        self.save_to_db()
+        app.logger.info('Email confirmed and updated DB: {}'.format(self.email_confirmed))
+        return self.email_confirmed
+
+
+    def send_confirmation_email(self):
+        if SES_email_confirmation(user_email=self.email):
+            app.logger.info('successfully sent email confirmation link to : {}'.format(self.email))
+            return True
+
         
 
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
-    #username = db.Column(db.Text, unique=True)
-    # password = db.Column(db.Text)
-    #is_active = db.Column(db.Boolean, default=True, server_default='true')
 
-    # @property
-    # def rolenames(self):
-    #     try:
-    #         return self.roles.split(',')
-    #     except Exception:
-    #         return []
+# class NN_Query(db.Model):
 
-    # @classmethod
-    # def lookup(cls, email):
-    #     return cls.query.filter_by(email=email).one_or_none()
+#     __tablename__ = "NN_Query"
 
-    # @classmethod
-    # def identify(cls, id):
-    #     return cls.query.get(id)
+#     id= db.Column(db.Integer,primary_key=True)
+#     #ToDo I need to assign the time stamp in the init not as class variable.
+#     user_email = db.Column(db.Integer, db.ForeignKey('User.email'))
+#     pdb_query = db.Column(db.String(length=8),nullable=True)
+#     query_time = db.Column(db.DateTime, index=True, nullable=False)
+#     query_inf = db.Column(db.Text(4294000000), nullable=True)
+#     query_email_sent = db.Column(db.Boolean, default=False)
 
-    # @property
-    # def identity(self):
-    #     return self.id
 
-    # def is_valid(self):
-    #     return self.is_active
 
-    # def __repr__(self):
-    #     return '<User {}>'.format(self.email)
+#     __table_args__ = CheckConstraint('NOT(pdb_query IS NULL AND query_inf IS NULL)'),
+
+
+#     def __init__(self,user_email,query=None, inference=None):
+#         self.user_email = user_email
+#         self.pdb_query = query
+#         self.query_time = datetime.now()
+#         # try:
+#         #     self.query_inf = inference.to_json(orient='index')
+#         # except Exception:
+#         #     app.logger.warning('Unable to generate inference json for {}'.format(self.pdb_query))
+
+#         if isinstance(inference,pd.DataFrame):
+#             app.logger.info('Inference Data frame was passed in as parameter for PDB {}'.format(self.pdb_query))
+#             self.query_inf = inference.to_json(orient='index')
+
+
+#     def __repr__(self):
+#         return "<NN_Query {}>".format(self.pdb_query)
+
+
+
+#     def save_to_db(self):
+#         db.session.add(self)
+#         db.session.commit()
+#         app.logger.info('Successfully saved query: {0} from user: {1} to DB'.format(self.pdb_query, self.user_email))
 
