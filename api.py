07f2e4ef37b3b59a2218341247e7153575a5d8c9
Danny Diaz <danny.jesus.diaz.94@gmail.com>
2021-09-25 20:24:20 -0500

    Got the CNN model running over flask; need to add celery

--- api.py
+++ api.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify, make_response
 from flask_restful import Api, Resource, reqparse
 
+import subprocess
+import sys
 
+from task import run_mutcompute
 nn_app = Flask(__name__)
 nn_api = Api(nn_app)
-
 
 class QueryAPI(Resource):
 
@@ -23,7 +25,13 @@ class QueryAPI(Resource):
         pdb_code = args.get('pdb_code').upper().strip()
         print("PDB Code:", pdb_code)
         if len(pdb_code) == 4:
+
+            out_csv = run_mutcompute(pdb_code, dir='/mutcompute/data/pdb_files', out_dir='/mutcompute/data/inference_CSVs', fs_pdb=False)
+
+            print(out_csv)
+
             return make_response(jsonify(Result='User requested pdb code: {}'.format(pdb_code)), 201)
+
         return make_response(jsonify(Result='User requested invalid pdb code: {}'.format(pdb_code.upper())), 400)
 
 
