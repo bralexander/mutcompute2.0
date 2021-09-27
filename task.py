dc2f9b0c68ee20aff1a8b1e7f8684d3101d95d83
Danny Diaz <danny.jesus.diaz.94@gmail.com>
2021-09-27 12:12:23 -0500

    Forgot to return dataframe in task.

--- task.py
+++ task.py
@@ -3,4 +3,4 @@ from scripts.run import gen_ensemble_inference
 
 def run_mutcompute(pdb_code, dir='/mutcompute/data/pdb_files', out_dir='/mutcompute/data/inference_CSVs', fs_pdb=False):
 
-    gen_ensemble_inference(pdb_code, dir='../data/pdb_files', out_dir='../data/inference_CSVs', fs_pdb=False)
+    return gen_ensemble_inference(pdb_code, dir='../data/pdb_files', out_dir='../data/inference_CSVs', fs_pdb=False)
