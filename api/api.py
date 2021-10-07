4e75eb2b11fc2c7a3da0fd63e68a758d12a4c053
Brad Alexander <bralexander@live.com>
2021-10-07 14:09:53 -0500

    moved main script here

--- api/api.py
+++ api/api.py
@@ -5,3 +5,7 @@ from app.models import Users
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
