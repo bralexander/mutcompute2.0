37e030d878cc077062722225741a18eb22f1de93
Brad Alexander <bralexander@live.com>
2021-10-14 15:38:14 -0500

    added routes for the paper

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import FAQ from "./components/faq"
 import Forgot from "./components/forgotPass"
 import Reset from "./components/resetPass"
 
+import PetaseWt from "./components/petaseWt"
 
 const  PrivateRoute = ({component: Component, ...rest }) => {
   const [logged] = useAuth();
@@ -38,6 +39,8 @@ function App() {
             <Route path="/login" exact component={() => <Login />} />
             <Route path="/viewer/:id" exact component={() => <Compvis3 />} />
             <Route path="/register" exact component={() => <Register />} />
+            <Route path="/petase/5xjh" exact component={() => <PetaseWt />} />
+            {/*<Route path="/petase/tk14" exact component={() => <PetaseThermo />} /> */}
             <PrivateRoute path="/nn" component={NNPage} />
             <Route path="/faq" exact component={() => <FAQ />} />
             <Route path="/forgot" exact component={() => <Forgot />} />
