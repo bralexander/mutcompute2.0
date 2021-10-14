10569ca76b2d5b33192fd1022d18df4ce331cb74
Brad Alexander <bralexander@live.com>
2021-10-14 15:40:29 -0500

    added components for paper links

---
+++ client/src/components/petaseThermo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Compvis3 from './compvis3'
+
+const PetaseThermo = () => {
+    return (
+        <div>
+        <Compvis3 id='6ij6'></Compvis3>
+        </div>
+    )
+}
+
+export default PetaseThermo
---
+++ client/src/components/petaseWt.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Compvis3 from './compvis3'
+
+const PetaseWt = () => {
+    return (
+        <div>
+        <Compvis3 id='5xjh'></Compvis3>
+        </div>
+    )
+}
+
+export default PetaseWt
