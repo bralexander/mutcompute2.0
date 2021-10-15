d34e9c5f350dd42b37414828a1a15dfe6fba37ee
Danny Diaz <danny.jesus.diaz.94@gmail.com>
2021-10-15 17:42:55 -0500

    Forgot to commit terms page.

---
+++ client/src/components/terms.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+ const Terms = () => {
+     return(
+         <div>
+             <p>
+                  By clicking on "Register" you agree to not misuse the Neural Network.
+             </p>
+             <p>
+                 This free web tool is for academic and educational use only. Only academic
+                 email addresses are able to make an account. 
+             </p>
+             <p>
+                 MutCompute is still an on going research project in the Ellington Research Lab
+                 and may be temporarily offline for updates and the addition of new features. We reserve the
+                 right to deny any user access to MutCompute without reason.
+                 For any questions or issues with MutCompute please contact
+                 <a href="mailto:danny.diaz@utexas.edu?subject=MutCompute Access>
+                     danny.diaz@utexas.edu.">
+                 </a>
+             </p>
+             <p>
+                 Please visit the <a href="/faq">FAQ</a> for additional details.
+             </p>
+         </div>
+     )
+ }
+
+ export default Terms
